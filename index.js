aa1f83fa431a2148f5e1c9a76cccb2a80c7dd44e
Karim <karim.ayoub.job@gmail.com>
2026-03-10 11:07:19 +0100

    GET et POST

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-import http from "node:http";
+// Express Application
 import express from "express";
 import "dotenv/config";
 
+// Port de l'application
 const cfg = { port: process.env.PORT || 3000 };
 
 const app = express();
+const formidable = require("formidable");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/", (req, res) => {
-  console.dir(req, { depth: null, color: true });
-  res.render("form", {
-    title: "Analyser les données GET",
-    data: req.query,
-  });
+// Middleware pour parser les données du formulaire
+app.use(express.urlencoded({ extended: true }));
+// Route pour afficher le formulaire
+app.all("/", (req, res) => {
+  if (req.method === "GET" || req.method === "POST") {
+    res.render("form", {
+      title: "Analyser les données GET",
+      data: req.body ? req.body : req.query,
+    });
+  } else {
+    next();
+  }
 });
 
+// Démarrer le serveur
 app.listen(cfg.port, () => {
   console.log(`Server is listening on port ${cfg.port}`);
 });
